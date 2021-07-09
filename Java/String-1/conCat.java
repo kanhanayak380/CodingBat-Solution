64bef3c95937b25a4da7d530a104da5df412b36a
kanhanayak380 <kanhanayak380@gmail.com>
2021-07-09 18:50:53 +0530

    Created conCat.java
    New Solution Added

---
+++ Java/String-1/conCat.java
@@ -0,0 +1,6 @@
+public String conCat(String a, String b) {
+  if((a.length() > 1 && b.length() > 0) && (a.charAt(a.length() - 1) == b.charAt(0))){
+    return a + b.substring(1);
+  }
+  return a + b;
+}
