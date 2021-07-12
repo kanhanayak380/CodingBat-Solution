f08a465b5220ceec840c62e8690775c32d7f6217
kanhanayak380 <kanhanayak380@gmail.com>
2021-07-12 20:27:21 +0530

    Created without2.java
    New Solution Added

---
+++ Java/String-1/without2.java
@@ -0,0 +1,9 @@
+public String without2(String str) {
+  if(str.length() > 2 && str.substring(0, 2).equals(str.substring(str.length() - 2))){
+    return str.substring(2);
+  }
+  if(str.length() == 2){
+    return "";
+  }
+  return str;
+}
